c71fd2969d340e8c1b5602c74643d9aa31bd71a7
jiho <jhhj424@naver.com>
2020-12-27 23:41:27 +0900

    vue.js 양방향 바인딩 test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Welcome to {{title2}}!</h1>
+    <h1>Welcome to {{title}}!</h1>
     <input type="text" v-model="input1" />
     <button type="button" @click="getData">Get</button>
     <button type="button" @click="setData">Set</button>
@@ -21,7 +21,7 @@
 export default {
   data() {
     return {
-      title: "개발자의 품격",
+      title: "나의 첫 Vue.js 애플리케이션 프로젝트",
       title2: "Seoul",
       input1: "abcd",
       options: [
